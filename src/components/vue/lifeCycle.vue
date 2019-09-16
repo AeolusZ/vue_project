cc2b08ef84ce307fed9929b43388c4bc88cf391b
ruxiu.zheng <18202764523@163.com>
2019-09-16 17:34:56 +0800

    添加vue生命周期页面

---
+++ src/components/vue/lifeCycle.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        打开控制台查看流程
+        <ol>
+            <li v-for="item in lifeCycleData" :key="item">{{item}}</li>
+        </ol>
+    </div>
+</template>
+<script>
+export default {
+    name: 'lifeCycle',
+    data(){
+        return {
+            lifeCycleData: []
+        }
+    },
+    beforeCreate(){
+        console.log('beforeCreate')
+        console.log('此时data还未加载')
+    },
+    created(){
+        console.log('created')
+        this.lifeCycleData.push('create钩子函数')
+    },
+    beforeMount(){
+        console.log('beforeMount')
+        this.lifeCycleData.push('beforeMount钩子函数')
+    },
+    mounted(){
+        console.log('mounted')
+        this.lifeCycleData.push('mounted钩子函数')
+    },
+    beforeUpdate(){
+        console.log('beforeUpdate', this.lifeCycleData)
+    },
+    updated(){
+        console.log('updated', this.lifeCycleData)
+    },
+    beforeDestroy(){
+        console.log('beforeDestroy')
+        this.lifeCycleData.push('beforeDestroy钩子函数')
+    },
+    destroyed(){
+        console.log('destroyed')
+        this.lifeCycleData.push('destroyed钩子函数')
+    }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import VuexStore from '@/components/vuex/vuexStore'
+import LifeCycle from '@/components/vue/lifeCycle'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/vuexStore',
       name: 'VuexStore',
       component: VuexStore
+    },
+    {
+      path: '/lifeCycle',
+      name: 'LifeCycle',
+      component: LifeCycle
     } 
   ]
 })
